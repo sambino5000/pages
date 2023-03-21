8d94715f0cfc4ffe0de2a795f759184f1936e9ff
samrock <samnev86@gmail.com>
2023-03-21 17:19:11 -0500

    check build5

---
+++ server/@qwik-city-plan.js
@@ -0,0 +1,1 @@
+import{n as e,o as r,q as l,m as t,r as n,l as o,t as u}from"./assets/@qwik-city-plan-ee411f09.js";export{e as basePathname,r as cacheModules,l as default,t as menus,n as routes,o as serverPlugins,u as trailingSlash};
---
+++ server/entry.ssr.js
@@ -0,0 +1,1 @@
+import"./assets/@qwik-city-plan-ee411f09.js";import{r as a}from"./assets/entry.ssr-7717898d.js";export{a as default};
